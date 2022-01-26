5160abdb522262f0e1ccecfbb29618252d8bcb7f
Alexandru Pisarenco <pisarenco.a@gmail.com>
2022-01-27 00:13:34 +0100

    Add gitpush.sh script

---
+++ gitpush.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -eo pipefail
+
+WORKSPACE="$1"
+
+pushd "${WORKSPACE}/gh-content"
+git config user.email "pisarenco.a@gmail.com"
+git config user.name "Alexandru Pisarenco"
+git checkout -b build
+if [ -n "$(git status --porcelain)" ] ; then
+    git add docs
+    git commit -m "Build: $(date -u '+%Y-%m-%dT%H:%M:%SZ')"
+fi
+git push origin build --force
+popd
