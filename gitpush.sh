10ff63584a606a6cdba388aba03d7e85bad816c1
Alexandru Pisarenco <pisarenco.a@gmail.com>
2022-01-27 00:57:27 +0100

    allow empty commits

--- gitpush.sh
+++ gitpush.sh
@@ -9,10 +9,11 @@ git config user.email "pisarenco.a@gmail.com"
 git config user.name "Alexandru Pisarenco"
 git rebase -s theirs origin/main
 CHANGES="$(git status --porcelain)"
+echo "Current changes:"
 echo $CHANGES
 if [ ! -n "$CHANGES" ] ; then
     git add docs
-    git commit -m "Build: $(date -u '+%Y-%m-%dT%H:%M:%SZ')"
+    git commit --allow-empty -m "Build: $(date -u '+%Y-%m-%dT%H:%M:%SZ')"
 fi
 git push origin build --force
 popd
