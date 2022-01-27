5ac8cace40939fae836730966d930a8bd87cb5da
Alexandru Pisarenco <pisarenco.a@gmail.com>
2022-01-27 01:01:26 +0100

    debug gitpush

--- gitpush.sh
+++ gitpush.sh
@@ -5,15 +5,19 @@ WORKSPACE="$1"
 CONTENT_DIR="$2"
 
 pushd "${WORKSPACE}/${CONTENT_DIR}"
+echo "File list:"
+ls -al .
 git config user.email "pisarenco.a@gmail.com"
 git config user.name "Alexandru Pisarenco"
 git rebase -s theirs origin/main
 CHANGES="$(git status --porcelain)"
 echo "Current changes:"
 echo $CHANGES
+echo "File list:"
+ls -al .
 if [ ! -n "$CHANGES" ] ; then
     git add docs
-    git commit --allow-empty -m "Build: $(date -u '+%Y-%m-%dT%H:%M:%SZ')"
+    git commit -m "Build: $(date -u '+%Y-%m-%dT%H:%M:%SZ')"
 fi
 git push origin build --force
 popd
